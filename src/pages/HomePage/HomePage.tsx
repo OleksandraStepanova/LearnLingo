c05b71dfa9aecc4e43a1502dda4faee176a1c7f1
oleksandra_stepanova <oleksandra.stepanova@gmail.com>
2025-01-09 23:45:18 +0100

    add styles for HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -9,15 +9,20 @@ export default function HomePage() {
   };
   return (
     <section>
-      <h1 className={css.title}>
-        Unlock your potential with the best <span>language </span>tutors
-      </h1>
-      <p>
-        Embark on an Exciting Language Journey with Expert Language Tutors:
-        Elevate your language proficiency to new heights by connecting with
-        highly qualified and experienced tutors.
-      </p>
-      <a onClick={handleButtonClick}>Get started</a>
+      <div className={css.wrapper}>
+        <h1 className={css.title}>
+          Unlock your potential with the best{' '}
+          <span className={css.span}>language</span> tutors
+        </h1>
+        <p className={css.text}>
+          Embark on an Exciting Language Journey with Expert Language Tutors:
+          Elevate your language proficiency to new heights by connecting with
+          highly qualified and experienced tutors.
+        </p>
+        <a className={css.navigate} onClick={handleButtonClick}>
+          Get started
+        </a>
+      </div>
     </section>
   );
 }

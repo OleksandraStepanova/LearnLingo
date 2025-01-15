333df17106679b7b38885a666df1998c3186c14e
oleksandra_stepanova <oleksandra.stepanova@gmail.com>
2025-01-15 21:13:31 +0100

    add info to cards of teachers

--- src/pages/TeachersPage/TeachersPage.tsx
+++ src/pages/TeachersPage/TeachersPage.tsx
@@ -5,6 +5,7 @@ import image2x from '../../img/teachers/janesmith@2x.png';
 import css from './TeachersPage.module.css';
 import { LuBookOpen } from 'react-icons/lu';
 import { BsStarFill } from 'react-icons/bs';
+import { CiHeart } from 'react-icons/ci';
 
 export default function TeachersPage() {
   return (
@@ -34,11 +35,51 @@ export default function TeachersPage() {
                       <p>Rating: 4.8</p>
                     </li>
                     <li>
-                      <p>Price / 1 hour: <span>30$</span></p>
+                      <p>
+                        Price / 1 hour: <span>30$</span>
+                      </p>
                     </li>
                   </ul>
-                  <button>+</button>
+                  <button className={css.favorites}>
+                    <CiHeart size={26} />
+                  </button>
                 </div>
+              </div>
+              <div className={css.text}>
+                <ul className={css.textList}>
+                  <li>
+                    <p>
+                      Speaks: <span>German, French</span>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Lesson Info:{' '}
+                      <span>
+                        Lessons are structured to cover grammar, vocabulary, and
+                        practical usage of the language.
+                      </span>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Conditions:{' '}
+                      <span>
+                        Welcomes both adult learners and teenagers (13 years and
+                        above).Provides personalized study plans
+                      </span>
+                    </p>
+                  </li>
+                </ul>
+                <button>Read more</button>
+              </div>
+              <div>
+                <ul className={css.heshtegs}>
+                  <li>#A1 Beginner</li>
+                  <li>#A2 Elementary</li>
+                  <li>#B1 Intermediate</li>
+                  <li>#B2 Upper-Intermediate</li>
+                </ul>
               </div>
             </div>
           </div>
